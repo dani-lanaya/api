9f8946d4db30ae6a4e430770e7e7b0d84c7e0de2
dani <danichesebrough@gmail.com>
2017-01-16 13:17:54 -0500

    Convert to kotlin.

---
+++ src/main/kotlin/com/jamsession/ApiApplication.kt
@@ -0,0 +1,11 @@
+package com.jamsession
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+open class ApiApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(ApiApplication::class.java, *args)
+}
